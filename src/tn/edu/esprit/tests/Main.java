aea316ae3d14d63c32fe57ea573d32d8fdab0eaf
houssemmeguebli <megbli.houssam@gmail.com>
2023-10-02 18:31:26 +0100

    demo

---
+++ src/tn/edu/esprit/tests/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.edu.esprit.tests;
+
+import tn.edu.esprit.tools.DataSource;
+
+/**
+ *
+ * @author megbl
+ */
+public class Main {
+      public static void main(String[] args) {
+        DataSource.getInstance();
+        DataSource.getInstance();
+        DataSource.getInstance();
+        DataSource.getInstance();
+        DataSource.getInstance();
+    }
+}
