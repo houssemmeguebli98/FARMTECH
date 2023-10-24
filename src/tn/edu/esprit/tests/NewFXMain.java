18a8b94a45522fd07805b891fa13516ddcc88377
houssemmeguebli98 <houssem.meguebl@esprit.tn>
2023-10-24 20:39:36 +0100

    version 2.0

--- src/tn/edu/esprit/tests/NewFXMain.java
+++ src/tn/edu/esprit/tests/NewFXMain.java
@@ -8,17 +8,13 @@ package tn.edu.esprit.tests;
 import java.io.IOException;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
- *
+ *  
  * @author megbl
  */
 public class NewFXMain extends Application {
@@ -26,56 +22,20 @@ public class NewFXMain extends Application {
   
    @Override
     public void start(Stage primaryStage) {
-                  /*
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("../gui/AjouterParcFXML.fxml"));
+      
+              try {
+            Parent root = FXMLLoader.load(getClass().getResource("../gui/WelcomePage.fxml"));
             Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("/*******Ajouter Parc /******");
+                           primaryStage.setScene(scene);
+                         primaryStage.setTitle("Liste de parc ");
             primaryStage.show();
         
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-            */
-          try {
-            Parent root = FXMLLoader.load(getClass().getResource("../gui/GetAllMessage.fxml"));
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("Liste des messages ");
-            primaryStage.show();
-        
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-       /*
-          try {
-            Parent root = FXMLLoader.load(getClass().getResource("../gui/AjouterMaterielFXML.fxml"));
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("Ajouter materiel ");
-            primaryStage.show();
-        
-        } catch (IOException ex) { 
-            System.out.println(ex.getMessage());
-        }
-       
-
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("../gui/TableAllMateriel.fxml"));
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("Liste des materiels ");
-            primaryStage.show();
-        
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-       */ 
            } 
     public static void main(String[] args) {
         launch(args);
     }
-    
-}
+    }
+
--- src/tn/edu/esprit/tools/DataSource.java
+++ src/tn/edu/esprit/tools/DataSource.java
@@ -19,7 +19,7 @@ public class DataSource {
     private Connection cnx;
     private static DataSource instance;
     
-    private String url = "jdbc:mysql://localhost/esprit";
+    private String url = "jdbc:mysql://localhost:3306/farmtech";
     private String user = "root";
     private String password = "";
     
