65c9506e6736682aa660749483fd2a2cf47fd874
houssemmeguebli98 <houssem.meguebl@esprit.tn>
2023-10-24 13:19:46 +0100

    version 1.3

--- src/tn/edu/esprit/tests/NewFXMain.java
+++ src/tn/edu/esprit/tests/NewFXMain.java
@@ -26,17 +26,53 @@ public class NewFXMain extends Application {
   
    @Override
     public void start(Stage primaryStage) {
-      
-          try {
-            Parent root = FXMLLoader.load(getClass().getResource("../gui/WelcomePage.fxml"));
+                  /*
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../gui/AjouterParcFXML.fxml"));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
-            primaryStage.setTitle("Liste de parc ");
+            primaryStage.setTitle("/*******Ajouter Parc /******");
             primaryStage.show();
         
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+            */
+          try {
+            Parent root = FXMLLoader.load(getClass().getResource("../gui/GetAllMessage.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Liste des messages ");
+            primaryStage.show();
+        
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+       /*
+          try {
+            Parent root = FXMLLoader.load(getClass().getResource("../gui/AjouterMaterielFXML.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Ajouter materiel ");
+            primaryStage.show();
+        
+        } catch (IOException ex) { 
+            System.out.println(ex.getMessage());
+        }
+       
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../gui/TableAllMateriel.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Liste des materiels ");
+            primaryStage.show();
+        
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+       */ 
            } 
     public static void main(String[] args) {
         launch(args);
